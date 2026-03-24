cfe394519e07e131e1396281dc76918c197d3b13
smontoya112 <141515975+smontoya112@users.noreply.github.com>
2026-03-24 17:56:14 -0500

    correcciones sobre publicher tcp, creo que esta listo, o con la prueba funciono y adelantar broker_tcp
    la prueba para ver que corriera era nc -l 8080 y funcionaba la conexcion

--- broker_tcp.c
+++ broker_tcp.c
@@ -1,6 +1,70 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+#include<netinet/in.h>
+#include<syscall.h>
+#include<unistd.h>
+#include <arpa/inet.h>
+
+//Implementar socket
+//Bind
+//listen
+//accept
+//read
+//connect
+//send/write a cada sub
+//close
+
+int crear_socket(){
+    int sock;
+    sock = syscall(SYS_socket, AF_INET, SOCK_STREAM, 0);
+    if (sock<0){
+        perror("Error al crear socket");
+        return 1;
+    }
+    return sock;
+}
+
+int bind_socket(int sock, struct sockaddr_in server_addr){
+    if (syscall(SYS_bind, sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Error al hacer bind");
+        return 1;
+    }
+    return 0;
+}
 
 int main(){
-    printf("holis");
+    
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    int sock = crear_socket();
+    if (bind_socket(sock, server_addr) < 0) {
+        return 1;
+    }
+    char[] mensajes[10][100];
+    while true{
+        int backlog = 10;
+        int listen_result = syscall(SYS_listen, sock, backlog);
+        if (listen_result < 0) {
+            perror("Error al escuchar");
+            return 1;
+        }
+        int accept_result = syscall(SYS_accept, sock, NULL, NULL);
+        if (accept_result < 0) {
+            perror("Error al aceptar conexión");
+            return 1;
+        }
+        char buffer[100];
+        ssize_t read_result = syscall(SYS_read, accept_result, buffer, sizeof(buffer));
+        if (read_result < 0) {
+            perror("Error al leer");
+            return 1;
+        }
+    }
+
+    
     return 0;
 }
--- publisher_tcp.c
+++ publisher_tcp.c
@@ -1,6 +1,79 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+#include<netinet/in.h>
+#include<syscall.h>
+#include<unistd.h>
+#include <arpa/inet.h>
+
+char* creadorMensaje(char equipo1,char equipo2){
+    char* mensajecompleto = malloc(100);
+
+    int x = rand() % 7;
+    int y = rand() % 20;
+    int z = rand() % 20;
+
+    switch(x){
+        case 0:
+            snprintf(mensajecompleto,100,"Gol de %c al equipo %c",equipo1,equipo2);
+            break;
+        case 1:
+            snprintf(mensajecompleto,100,"Gol de %c al equipo %c",equipo2,equipo1 );
+            break;
+        case 2:
+            snprintf(mensajecompleto,100,"Cambio: jugador %d entra por %d",y,z);
+            break;
+        case 3:
+            snprintf(mensajecompleto,100,"Tarjeta amarilla para %d del equipo %c",y,equipo1);
+            break;
+        case 4:
+            snprintf(mensajecompleto,100,"Tarjeta roja para %d del equipo %c",y,equipo1);
+            break;
+        case 5:
+            snprintf(mensajecompleto,100,"Tarjeta amarilla para %d del equipo %c",y,equipo2);
+            break;
+        case 6:
+            snprintf(mensajecompleto,100,"Tarjeta roja para %d del equipo %c",y,equipo2);
+            break;
+    }
+
+    return mensajecompleto;
+}
 
 int main(){
-    printf("holis");
+    srand(time(NULL));
+    printf("Ingrese los equipos que se enfrentan: ");
+    char equipo1, equipo2;
+    int num_mensajes= rand() % 10 + 1;
+    
+    scanf(" %c %c", &equipo1, &equipo2);
+
+    // INICIO DE ENVIO DE MENSAJES POR TCP
+    int sock;
+    sock = syscall(SYS_socket, AF_INET, SOCK_STREAM, 0);
+    if (sock<0){
+        perror("Error al crear socket");
+        return 1;
+    }
+    
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    int conectado = syscall(SYS_connect,sock,(struct sockaddr *)&server_addr, sizeof(server_addr));
+    
+    if(conectado<0){
+        perror("Error al conectar");
+        return 1;
+    }
+
+    for(int i=0; i<num_mensajes; i++){
+        char* mensaje = creadorMensaje(equipo1,equipo2);
+        syscall(SYS_write, sock, mensaje, strlen(mensaje));
+        printf("Mensaje enviado %d: %s\n", i+1, mensaje);
+        free(mensaje);
+    }
+    syscall(SYS_close, sock);
     return 0;
 }
